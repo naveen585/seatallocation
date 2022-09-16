9cb4c90a4f6025dcf5f9d06795f60ce4481c04f8
utsavmori <utsavrmori@gmil.com>
2022-09-16 23:37:46 +0530

    initialize project

---
+++ src/main/java/com/cs/seatallocation/SeatallocationApplication.java
@@ -0,0 +1,13 @@
+package com.cs.seatallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatallocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatallocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs/seatallocation/SeatallocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.seatallocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatallocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
